d721d0d0c03e5b674ddbf6e63c73dedecec5732b
stanley zhou <riematrix@users.noreply.github.com>
2014-08-23 20:58:47 +0800

    basic display of a zip with jstree

--- zipexplorer.js
+++ zipexplorer.js
@@ -1,16 +1,41 @@
 /**
  * Created by riematrix on 2014/8/21.
  */
-var DataAdapter = {
-    zipToTreeObject: function(zip){
+function ZipExplorer(zipData){
+    this.zip = new JSZip(DataStorage.get(zipData));
+}
+ZipExplorer.prototype = {
+    explore: function(){},
+    createFile: function(path, data){
+        var content = data || "";
+        return this.zip.file(path, content);
+    },
+    readFile: function(path){
+        return this.zip.file(path);
+    },
+    createFolder: function(path){
+        return this.zip.folder(path);
+    },
+    update: function(path,data){},
+    remove: function(path){
+        return this.zip.remove(path);
+    },
+
+    getRootZipObject: function(){
+        return this.zip;
+    },
+
+    rootZipToTreeObject: function(){
+        var zipObject = this.zip;
         var root = {
             id: "root",
             text: "root",
             state: ""
         };
         var convert = function(dir, node){
-            var files = zip.file(/[\s\S]/),
-                folders = zip.folder(/[\s\S]/),
+            var current = zipObject.folder(dir);
+            var files = current.file(/[\s\S]/),
+                folders = current.folder(/[\s\S]/),
                 name,
                 children = [],
                 i;
@@ -30,14 +55,32 @@ var DataAdapter = {
                 name = folders[i].name;
                 if(dir === "" && name.match(/\//g).length === 1
                     || name.replace(dir).match(/\//g).length === 1){
-                    children.push({
+                    var folder = {
                         id: name,
                         text: name.substring(0,name.lastIndexOf("/")).split("/").pop()
-                    });
+                    };
+                    convert(name, folder);
+                    children.push(folder);
                 }
             }
         };
         convert("", root);
         return root;
+    }
+};
+
+var DataStorage = {
+    get: function(keyStore){
+        if(typeof keyStore === "object"){}
+        else{
+            keyStore += "";
+        }
+        return null;
+    },
+    put: function(key, value){
+        if(typeof key === "object"){}
+        else{
+            key += "";
+        }
     }
 };
