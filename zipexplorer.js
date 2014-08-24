3cbc26187900c5dc2857e146ac96376d3bd62c9e
stanley zhou <riematrix@users.noreply.github.com>
2014-08-24 23:10:21 +0800

    update icon/id after rename file

--- zipexplorer.js
+++ zipexplorer.js
@@ -1,11 +1,18 @@
 /**
  * Created by riematrix on 2014/8/21.
  */
-function ZipExplorer(zipData){
+function ZipExplorer(zipData, renderTarget){
     this.zip = new JSZip(DataStorage.get(zipData));
+    this.tree = this.rootZipToTreeObject();
+    if(renderTarget) this.explore(renderTarget);
 }
 ZipExplorer.prototype = {
-    explore: function(){},
+    explore: function(renderTarget){
+        var target = typeof renderTarget === "string"
+            ? $("#" +renderTarget)
+            : $(renderTarget);
+        target.jstree(this.defaultTheme);
+    },
     createFile: function(path, data){
         var content = data || "";
         return this.zip.file(path, content);
@@ -65,7 +72,117 @@ ZipExplorer.prototype = {
             }
         };
         convert("", root);
+        this.defaultTheme.core.data = this.root;
         return root;
+    }
+};
+ZipExplorer.prototype.defaultTheme = {
+    "core" : {
+        "animation" : 0,
+        "check_callback" : true,
+        "themes" : { "stripes" : true,'variant' : 'small' }
+    },
+    'sort' : function(a, b) {
+        return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
+    },
+    'contextmenu' : {
+        'items' : function(node) {
+            var tmp = $.jstree.defaults.contextmenu.items();
+            delete tmp.create.action;
+            tmp.create.label = "New";
+            tmp.create.submenu = {
+                "create_folder" : {
+                    "separator_after"	: true,
+                    "label"				: "Folder",
+                    "action"			: function (data) {
+                        var inst = $.jstree.reference(data.reference),
+                            obj = inst.get_node(data.reference);
+                        inst.create_node(obj, { type : "default", text : "New folder" }, "last", function (new_node) {
+                            setTimeout(function () { inst.edit(new_node); },0);
+                        });
+                    }
+                },
+                "create_file" : {
+                    "label"				: "File",
+                    "action"			: function (data) {
+                        var inst = $.jstree.reference(data.reference),
+                            obj = inst.get_node(data.reference);
+                        inst.create_node(obj, { type : "file", text : "New file" }, "last", function (new_node) {
+                            setTimeout(function () { inst.edit(new_node); },0);
+                        });
+                    }
+                }
+            };
+            if(this.get_type(node) === "file") {
+                delete tmp.create;
+            }
+            return tmp;
+        }
+    },
+    'types' : {
+        'default' : { 'icon' : 'folder' },
+        'file' : { 'valid_children' : [], 'icon' : 'file' }
+    },
+    'plugins' : ['state','dnd','sort','types','contextmenu','unique']
+};
+ZipExplorer.prototype.controller = {
+    'delete_node.jstree': function(e, data){},
+    'create_node.jstree': function(e, data){},
+    'rename_node.jstree': function(e, data){},
+    'move_node.jstree': function(e, data){},
+    'copy_node.jstree': function(e, data){},
+    'changed.jstree': function(e, data){},
+    'dblclick.jstree': function(e, data){}
+};
+
+function FileDecorator(fileName, data){
+    if(fileName && data)
+    {
+        var end_fix = fileName.split(".").pop();
+        return this.decorate(end_fix, data);
+    }
+    return this;
+}
+FileDecorator.prototype = {
+    decorators: {
+        "image": function(data){
+            return data;
+        },
+        "css": function(data){
+            return data;
+        },
+        "javascript": function(data){
+            return data;
+        },
+        "html": function(data){
+            return data;
+        },
+        "text": function(data){
+            return data;
+        }
+    },
+    decoratorMapping: function(){
+        this.decoratorMapping = {
+        'png': this.decorators["image"],
+        'jpg': this.decorators["image"],
+        'jpeg': this.decorators["image"],
+        'bmp': this.decorators["image"],
+        'gif': this.decorators["image"],
+        'css': this.decorators["css"],
+        'html': this.decorators["html"],
+        'js': this.decorators["javascript"],
+        'txt': this.decorators["javascript"]
+    }
+    },
+    extend: function(end_fix, decorator){
+        this.decoratorMapping[end_fix] = decorator;
+    },
+    decorate: function(end_fix, data){
+        var decorator = this.decoratorMapping[end_fix];
+        if(typeof decorator === "function"){
+            return decorator(data);
+        }
+        return data;
     }
 };
 
